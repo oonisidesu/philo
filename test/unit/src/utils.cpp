77d60440feb8c0f12a481480e5550e7686b6df73
oonisidesu <eeeelllleeee24@gmail.com>
2024-05-13 15:56:45 +0900

    chore: philosopher.c と ft_atoi.c を単体テスト ソースに追加する

--- test/unit/src/utils.cpp
+++ test/unit/src/utils.cpp
@@ -35,3 +35,31 @@ TEST(GetTimeDiffTest, ReturnsCorrectDifference)
 	diff = get_time_diff(&start, &end);
 	EXPECT_EQ(diff, 700); // 期待される差は700ミリ秒です。
 }
+
+TEST(LogPhilosopherActionTest, LogsCorrectMessage)
+{
+	t_philosopher	philo;
+	FILE			*temp_stdout;
+	FILE			*temp;
+	char			output[100];
+	char			expected_output[100];
+
+	// philoの初期化...
+	// stdoutを一時ファイルにリダイレクトします。
+	temp_stdout = stdout;
+	stdout = fopen("temp.txt", "w");
+	// テスト対象の関数を呼び出します。
+	log_philosopher_action(&philo, "is thinking");
+	// stdoutを元に戻します。
+	fclose(stdout);
+	stdout = temp_stdout;
+	// 一時ファイルから出力を読み取ります。
+	temp = fopen("temp.txt", "r");
+	fgets(output, 100, temp);
+	fclose(temp);
+	// 出力が期待通りであることを確認します。
+	// ここでは、哲学者のIDとアクションが正しくログに記録されていることを確認します。
+	// タイムスタンプはテスト実行時に依存するため、このテストでは確認しません。
+	sprintf(expected_output, " %d is thinking\n", philo.id);
+	EXPECT_TRUE(strstr(output, expected_output) != NULL);
+}
